50ce8b3262c8936896a0966b1cd38de99c643ebc
unknown <shobhithaas18@gmail.com>
2024-08-12 18:57:31 +0530

    changes

--- Backend/Controller/AdminController.js
+++ Backend/Controller/AdminController.js
@@ -10,57 +10,11 @@ exports.getEvents = async (req, res) => {
     res.status(500).json({ message: "Internal server error" });
   }
 };
- 
 
- 
- 
 
-// app.post("/upload-image", upload.single("image"), async (req, res) => {
-//   const { title, description, location, date, price } = req.body;
-//   const imageDetails = req.file.filename;
-//   console.log(imageDetails);
-//   try {
-//     await Image.create({
-//       image: imageDetails,
-//     });
-//     res.json({ status: "ok" });
-//   } catch (error) {
-//     res.json({ status: "not-ok" });
-//   }
-// });
 
-// exports.addEvents = async (req, res) => {
-//   try {
-//     const { title, description, location, date, price } = req.body;
 
-//     if (!req.file) {
-//       return res.status(400).json({ message: "Image file is required" });
-//     }
 
-//     const dateParsed = new Date(date);
-
-//     if (isNaN(dateParsed.getTime())) {
-//       return res.status(400).json({ message: "Invalid Date" });
-//     }
-
-//     const newEvent = await Event.create({
-//       title,
-//       description,
-//       location,
-//       date: dateParsed,
-//       price,
-//       image: {
-//         data: fs.readFileSync("public/Images/" + req.file.filename),
-//         contentType: "image/png", // Adjust accordingly based on file type
-//       },
-//     });
-
-//     res.status(200).json({ data: newEvent, message: "Successfully created" });
-//   } catch (error) {
-//     console.log(error);
-//     res.status(500).json({ message: "Internal server error" });
-//   }
-// };
 
 exports.deleteEvents = async (req, res) => {
   try {
@@ -75,16 +29,16 @@ exports.deleteEvents = async (req, res) => {
     res.status(500).json({ message: "Internal server error" });
   }
 };
- 
+
 function formatDate(dateString) {
   const [year, month, day] = dateString.split("-");
   return `${day}-${month}-${year}`;
 }
 
- 
+
 function parseDate(dateString) {
   const [day, month, year] = dateString.split("-");
-  return new Date(year, month - 1, day);  
+  return new Date(year, month - 1, day);
 }
 
 exports.updateEvent = async (req, res) => {
@@ -99,18 +53,18 @@ exports.updateEvent = async (req, res) => {
       return res.status(404).json({ message: "Event not found" });
     }
 
- 
+
     eventToUpdate.title = title;
     eventToUpdate.description = description;
     eventToUpdate.location = location;
-    eventToUpdate.date = formattedDate; 
-   
- 
+    eventToUpdate.date = formattedDate;
+
+
     if (imageDetails) {
       eventToUpdate.image = imageDetails;
     }
 
-   
+
     const updatedEvent = await eventToUpdate.save();
     res.status(200).json({ data: updatedEvent, message: "Event updated successfully" });
   } catch (error) {
